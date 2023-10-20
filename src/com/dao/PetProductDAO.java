d14d03c9d16cd35681063cea42344122426fc4a8
DUC PHU <DUC PHU@FX506LH>
2023-10-20 15:07:51 +0700

    Merge origin/master
    Conflicts:
    	src/com/bus/CustomerBUS.java

--- src/com/dao/PetProductDAO.java
+++ src/com/dao/PetProductDAO.java
@@ -31,5 +31,94 @@ public class PetProductDAO {
         conn = MyConnection.getConnection();
     }
     
+    public void add(PetProductDTO product)
+    {
+        try{
+            String qry = "insert into PetProduct value ("
+                    + "'" + product.getId() + "'" 
+                    + ", "+ "N'" + product.getName() + "'" 
+                    + ", "+ "" + product.getSoldPrice() + "" 
+                    + ", "+ "" + product.getImportPrice() + ""
+                    + ", "+ "N'" + product.getDescription() + "'"
+                    + ", "+ "'" + product.getSupplierId() + "'"
+                    + ", "+ "'" + product.getCategoryID() + "'"
+                    + ", "+ "" + product.getInStock()+ ""
+                    + ")";
+            stmt = conn.createStatement();
+            int rowsAffected = stmt.executeUpdate(qry);
+            if (rowsAffected == 1) {
+                JOptionPane.showMessageDialog(null, "Thêm sản phẩm thành công!");
+            } else {
+                JOptionPane.showMessageDialog(null, "Thêm sản phẩm thất bại!");
+            }
+        }
+        catch(SQLException ex)
+        {
+            
+        }
+    }
+    
+    public void delete(String ID)
+    {
+        try{
+            String qry = "delete from PetProduct "
+                    + " where ID ='" +ID+ "'";
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(qry);
+        }
+        catch (SQLException ex)
+        {
+            
+        }
+    }
+    
+    public void edit(PetProductDTO product)
+    {
+        try{
+            String qry = "Update PetProduct "
+                    + " set "
+                    + " petName = N'" +product.getName()+"', "
+                    + " soldPrice = " +product.getSoldPrice()+", "
+                    + " importPrice = " +product.getImportPrice()+", "
+                    + " Description = N'" +product.getDescription()+"', "
+                    + " supplierID = '" +product.getSupplierId()+"', "
+                    + " categoryID = '" +product.getCategoryID()+"', "
+                    + " inStock = " +product.getInStock()+"";
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(qry);
+        }
+        catch (SQLException ex)
+        {
+            
+        }
+    }
+    
+    public ArrayList readPetProductList()
+    {
+        ArrayList list = new ArrayList<PetProductDTO>();
+        try{
+            String qry = "Select * from PetOnStore";
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(qry);
+            while(rs.next())
+            {
+                PetProductDTO product = new PetProductDTO();
+                product.setId(rs.getString("petID"));
+                product.setName(rs.getString("petName"));
+                product.setSoldPrice(Long.parseLong(rs.getString("soldPrice")));
+                product.setImportPrice(Long.parseLong(rs.getString("importPrice")));
+                product.setDescription(rs.getString("Description"));
+                product.setSupplierId(rs.getString("supplierID"));
+                product.setCategoryID(rs.getString("categoryID"));
+                product.setInStock(Integer.parseInt(rs.getString("inStock")));
+                list.add(product);
+                
+            }
+        }
+        catch(SQLException ex){
+            
+        }
+        return list;
+    }
     
 }
