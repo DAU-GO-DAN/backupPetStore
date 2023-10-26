e3429c2f7a9c8a3fc57eca69239416b747a98337
huynh <huynh@DuongTienSinh>
2023-10-27 01:50:33 +0700

    Merge origin/master
    Conflicts:
    	src/com/dao/abc.java

--- src/com/dao/SupplierDAO.java
+++ src/com/dao/SupplierDAO.java
@@ -31,5 +31,76 @@ public class SupplierDAO {
         conn = MyConnection.getConnection();
     }
     
+    public void add(SupplierDTO sup){
+        try{
+            String qry = "insert into Supplier values ('" + sup.getId() + 
+                    "', N'" + sup.getName() + 
+                    "', '" + sup.getPhone() + 
+                    "', N'" + sup.getAddress() + "')";
+            stmt = conn.createStatement();
+            int rowAffected = stmt.executeUpdate(qry);
+            if(rowAffected == 1){
+                JOptionPane.showMessageDialog(null, "Thêm thành công");
+            }else{
+                JOptionPane.showMessageDialog(null, "Thêm không thành công");
+            }
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Thêm không thành công");
+        }
+    }
+    
+    public void delete(String id){
+        try{
+            String qry = "delete from Supplier where id = '" + id + "'";
+            stmt = conn.createStatement();
+            int rowAffected = stmt.executeUpdate(qry);
+            if(rowAffected == 1){
+                JOptionPane.showMessageDialog(null, "Xóa thành công");
+            }else{
+                JOptionPane.showMessageDialog(null, "Xóa không thành công");
+            }
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Xóa không thành công");
+        }
+    }
+    
+    public void edit(SupplierDTO sup ,String id){
+        try{
+            String qry = "update Supplier set "
+                    + "name = '" + sup.getName()+ "', "
+                    + "phone = N'" + sup.getPhone()+ "', "
+                    + "address = N'" + sup.getAddress()+ "'"
+                    + "where id = '" + id + "'";
+            stmt = conn.createStatement();
+            int rowAffected = stmt.executeUpdate(qry);
+            if(rowAffected == 1){
+                JOptionPane.showMessageDialog(null, "Sửa thành công");
+            }else{
+                JOptionPane.showMessageDialog(null, "Sửa không thành công");
+            }
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Sửa không thành công");
+        }
+    }
+    
+    public ArrayList readData(){
+        ArrayList<SupplierDTO> supList = new ArrayList<>();
+        try{
+            String qry = "select * from Supplier";
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(qry);
+            while(rs.next()){
+                SupplierDTO supTemp = new SupplierDTO();
+                supTemp.setId(rs.getString("id"));
+                supTemp.setName(rs.getString("name"));
+                supTemp.setPhone(rs.getString("phone"));
+                supTemp.setAddress(rs.getString("address"));
+                supList.add(supTemp);
+            }
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Tải dữ liệu không thành công");
+        }
+        return supList;
+    }
     
 }
