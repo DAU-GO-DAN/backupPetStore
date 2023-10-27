64b255eaa293925e9cb5b3afbe625b7f4f5aa37c
DUC PHU <DUC PHU@FX506LH>
2023-10-27 13:41:10 +0700

    Merge origin/master
    Conflicts:
    	src/com/bus/CustomerBUS.java

--- src/com/bus/PetOnStoreBUS.java
+++ src/com/bus/PetOnStoreBUS.java
@@ -4,10 +4,71 @@
  */
 package com.bus;
 
+
+import com.dao.PetOnStoreDAO;
+import com.dao.PetOnStoreDTO;
+import java.util.ArrayList;
+
 /**
  *
  * @author huynh
  */
 public class PetOnStoreBUS {
+    ArrayList<PetOnStoreDTO> petList;
+    static PetOnStoreDAO petData = new PetOnStoreDAO();
+
+    public PetOnStoreBUS() {
+        
+    }
     
+    public ArrayList readDAO()
+    {
+        return petData.readPOSList();
+    }
+    
+    void readData()
+    {
+        if(petList == null)
+        {
+            petList = new ArrayList<PetOnStoreDTO>();
+        }
+        petList = petData.readPOSList();
+    }
+    
+    void add(PetOnStoreDTO pet)
+    {
+        petData.add(pet);
+        petList.add(pet);
+    }
+    
+    void delete(String ID)
+    {
+        petData.delete(ID);
+        petList.removeIf(pet ->pet.getId().equals(ID));
+    }
+    
+    void edit(PetOnStoreDTO pet)
+    {
+        petData.edit(pet);
+        petList.removeIf(petTemp -> petTemp.getId().equals(pet.getId()));
+        petList.add(pet);
+    }
+    
+    ArrayList<PetOnStoreDTO> search(String text)
+    {
+        ArrayList<PetOnStoreDTO> matchingPets = new ArrayList<>();
+        for(PetOnStoreDTO pet : petList)
+        {
+            if(pet.getId().contains(text) || pet.getName().contains(text))
+            {
+                matchingPets.add(pet);
+            }
+        }
+        return matchingPets;
+    }
+    
+    public String generateID()
+    {
+        String prefix = "";
+    }
 }
